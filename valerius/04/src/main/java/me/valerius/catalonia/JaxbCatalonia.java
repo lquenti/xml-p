31e1726f8edc70777013e43efe5372a6b40dce1b
Valerius Mattfeld <mail@valerius.me>
2025-01-14 14:04:39 +0100

    add xjc

---
+++ valerius/04/src/main/java/me/valerius/catalonia/JaxbCatalonia.java
@@ -0,0 +1,11 @@
+package me.valerius.catalonia;
+
+
+
+
+class JaxbCatalonia {
+    public static void main(String[] args) {
+        System.out.println("JaxbCatalonia.main()");
+        var airport = new Airport();
+    }
+}
