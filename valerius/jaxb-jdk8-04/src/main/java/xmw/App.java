ebb1943699f79563d7b475075c659cebb60979f8
Valerius Mattfeld <mail@valerius.me>
2025-01-14 15:12:04 +0100

    update jaxb versioning and stuff

---
+++ valerius/jaxb-jdk8-04/src/main/java/xmw/App.java
@@ -0,0 +1,63 @@
+package xmw;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javax.xml.XMLConstants;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.Source;
+import javax.xml.transform.sax.SAXSource;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+import javax.xml.parsers.SAXParserFactory;
+
+import xmw.mondial.Mondial;
+
+public class App {
+    public static void main(String[] args) {
+        try {
+            // Create JAXB context for the Mondial class
+            JAXBContext context = JAXBContext.newInstance(Mondial.class);
+
+            File inputFile = new File("mondial.xml");
+            if (!inputFile.exists()) {
+                throw new FileNotFoundException("not there." + inputFile.getAbsolutePath());
+            }
+
+            System.setProperty("javax.xml.accessExternalDTD", "all");
+
+            // Create unmarshaller to read XML
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+
+            Mondial mondial = (Mondial) unmarshaller.unmarshal(inputFile);
+
+            // Create marshaller to write XML
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshaller.setProperty("com.sun.xml.bind.xmlDeclaration", false);
+            marshaller.setProperty("com.sun.xml.bind.xmlHeaders",
+                    "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<!DOCTYPE mondial SYSTEM \"mondial.dtd\">");
+
+            // Write to output XML file
+            File outputFile = new File("output.xml");
+            marshaller.marshal(mondial, outputFile);
+
+            System.out.println("XML file has been copied successfully to output.xml");
+
+        } catch (FileNotFoundException e) {
+            System.err.println("Input file not found: " + e.getMessage());
+            e.printStackTrace();
+        } catch (JAXBException e) {
+            System.err.println("Error processing XML: " + e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("Unexpected error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
